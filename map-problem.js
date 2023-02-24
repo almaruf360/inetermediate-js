fbe0e0a5e100b0828edc150cb7a166c633637a5d
Maruf360 <sayedalmaruf3@gmail.com>
2023-02-25 02:18:36 +0600

    problem done

--- map-problem.js
+++ map-problem.js
@@ -1,4 +1,8 @@
-const oddArray = [1,3,5,7,9] ;
-const makeEven =oddArray.map(n => n+1);
-console.log(makeEven)
+// const oddArray = [1,3,5,7,9] ;
+// const makeEven =oddArray.map(n => n+1);
+// console.log(makeEven)
 
+
+const nums =[1,2,3,4,5];
+let output = nums.filter(n => n%2);
+console.log(output)
