615aaea972a6b77395b3fc210d039678a3d575c6
Maruf360 <sayedalmaruf3@gmail.com>
2023-02-27 02:15:42 +0600

    all added

--- problem-2.js
+++ problem-2.js
@@ -1,5 +1,9 @@
-const a =[-2];
-const b =[72.8621];
 
-const result = a * b;
-console.log(result)
+// (x===5)? console.log('apni thik bolsen') : console.log('apni kisu janen na mia, hudai pechal paren');
+
+
+const friends = ['maruf','shamim','afzal','mumin'];
+
+friends.map((names,index) => {
+    console.log(names.length, index);
+})
